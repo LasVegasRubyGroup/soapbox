46e6a18b2f95a4c2e0f369f0e4948619d83cd148
Judd Lillestrand <juddl333@gmail.com>
2013-05-16 13:06:19 -0700

    move app secret token to heroku config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Soapbox::Application.config.secret_token = 'a24c14fbbd7b61f1734e042eacedc50312e26396e7242e803f9aa7b1e37f5c63b1251460d7ac63660157803e7dd4fbdbf6940998a37c83f0bb3e4a3564dfb5f5'
+Soapbox::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
