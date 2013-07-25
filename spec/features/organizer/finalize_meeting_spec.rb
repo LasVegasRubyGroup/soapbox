0e82e5a2d17c8f44e9a4be30b366dd8f23ed1bee
Mark Schifflin <rschifflin@hotmail.com>
2013-07-25 09:28:05 -0700

    Defining user experience

---
+++ spec/features/organizer/finalize_meeting_spec.rb
@@ -0,0 +1,53 @@
+require 'features/features_helper'
+describe "A meeting detail page, with 3 time slots, each with a topic" do
+  include_context "full meeting setup"
+
+  context "When viewed by an organizer" do
+    let(:organizer) { create(:organizer) }
+    before do 
+      signin_as(organizer)
+      visit meeting_path(meeting)
+    end
+ 
+    it "contains a 'create meeting' link" do
+      expect(page).to have_link("Create Meeting")
+    end
+
+    it "contains an 'edit meeting' link" do
+      expect(page).to have_link("Edit Meeting")
+    end
+
+    it "contains a 'finalize meeting' link" do
+      expect(page).to have_link("Finalize")
+    end
+
+    it "contains an 'open kudos' link" do
+      expect(page).to have_link("Open this Meeting for Voting")
+    end
+
+    context "with one freshman presenter" do
+      before do
+        TimeSlot.create(presenter_id: presenter_two.id) 
+        TimeSlot.create(presenter_id: presenter_three.id) 
+        click_link("Finalize")
+      end
+
+      it "awards bonus points to the freshman presenter" do
+        pending
+        expect(page).to have_text("Russ Smith awarded 10 bonus points!")
+      end
+    end
+
+    context "when the meeting is closed" do 
+      before do
+        meeting.update_attributes(state: 'closed')
+      end
+
+      it "should display a graph" do 
+        visit meeting_path(meeting)
+        page.should have_selector("//h2", text: 'Kudos Awarded')
+      end
+    end   
+
+  end
+end
--- spec/support/omniauth_helpers.rb
+++ spec/support/omniauth_helpers.rb
@@ -1,19 +1,20 @@
 module OmniauthHelpers
-  def set_omniauth_for(user)
+  def set_omniauth_for(user, is_organizer)
     OmniAuth.config.test_mode = true
-    auth_hash = {
+    organizer_hash = is_organizer ? {"role" => "Organizer" } : {}
+    auth_hash = { 
       'provider' => 'meetup',
       'uid' => user.uid,
       'info' => { 'name' => user.name },
       'extra' => {}
     }
     OmniAuth.config.mock_auth[:meetup] = OmniAuth::AuthHash.new(auth_hash)
-    Meetup::Profile.stub(:get).and_return({})
+    Meetup::Profile.stub(:get).and_return(organizer_hash)
   end
 
   def signin_as(user)
     visit root_path
-    set_omniauth_for user
+    set_omniauth_for user, user.organizer 
     click_link 'Sign In'
   end
 end
