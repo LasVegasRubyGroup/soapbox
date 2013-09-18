5a947f7824f704c515400c382fe2385b21c31b4d
Brian V. Hughes <brian@brianvhughes.com>
2013-09-18 16:46:32 -0700

    Add notes for the Award class.

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,15 @@
 class Award
+
+  # Award { topic: topic_id,
+        #   user: user_id,
+        #   base: 5,
+        #   presenter: 3/4 votes,
+        #   suggester: 1/4 votes,
+        #   kudos: kudo #,
+        #   bonus: 10 if 1st time
+        # }
+        # Reasons will be stored as a serialized hash. Likely to be
+        # turned into a join-table model.
+  
   attr_accessor :total_points, :suggestion_points, :presentation_points, :kudos_points
 end
